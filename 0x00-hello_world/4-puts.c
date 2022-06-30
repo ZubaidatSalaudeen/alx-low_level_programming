55316bae213aca0bb15b0e7d406a3778d84c64a5
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-06-30 09:49:04 +0000

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* using puts instead of printf */
+
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+
+	return 0;
+}
