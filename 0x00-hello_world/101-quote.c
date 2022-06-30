74079b5de0adee59b341cd0945ebece5f8229254
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-06-30 21:48:48 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,16 @@
 #include<unistd.h>
 /**
  * main - Entry point
+ *
  * Description: Dora Korpar's quote
- * Return 1 (Success)
+ *
+ * Return: 1 (Success)
  */
 int main(void)
 {
 	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
 	int length = strlen(text);
-	
-	write(2, text, length);
 
+	write(2, text, length);
 	return (1);
-}	
+}
