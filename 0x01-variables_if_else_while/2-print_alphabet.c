37617c3395bd285c43fab1952ed91e5ac865656e
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-01 20:43:38 +0000

    print alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program to print english alphabets in lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
