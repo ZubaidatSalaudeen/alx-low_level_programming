017298d0eae9af0e201845d52e8d4218817a0e35
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-05 15:56:33 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
+
