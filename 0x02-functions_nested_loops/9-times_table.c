4d01bce7a323974d700336e9d3f6864cb4bb680b
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-05 21:33:53 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,12 +7,13 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int k = i * j;
+	int k;
 
 	for (i = 0; i < 10; i++)
 	{
 	for (j = 0; j < 10; j++)
 	{
+	k = i * j;
 	if (k == 0)
 	{
 	_putchar(k + '0');
