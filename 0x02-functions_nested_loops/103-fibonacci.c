389d4db7632c6a7d87b5d7363f060207318509b0
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-06 01:18:40 +0000

    fabronacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Add even values of fabonacci
+ * Return: 0for Success
+ */
+int main(void)
+{
+	int i = 1, j = 2, sum = 0;
+	int k;
+
+	while (j < 4000000)
+	{
+	if (j % 2 == 0)
+	sum += j;
+	k = j;
+	j += i;
+	i = k;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
