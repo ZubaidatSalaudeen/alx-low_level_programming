749f0a0df05127aba16b5ba5018e304fd842c759
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-06 23:42:44 +0000

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/** main - program to test funtion that prints +ve or -ve integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+#include <stdio.h>
+void positive_or_negative(int i);
+
+#endif
