9bedb6e5a204da0e5b024044dff8dcf914e6780e
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-06 00:02:29 +0000

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -42,7 +42,7 @@ void print_times_table(int n)
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
 	if (j != n)
-	_putchar(' ');
+	_putchar(',');
 	}
 	else
 	{
@@ -51,7 +51,7 @@ void print_times_table(int n)
 	_putchar(((k / 10) % 10) + '0');
 	_putchar((k % 10) + '0');
 	if (j != n)
-	_putchar(' ');
+	_putchar(',');
 	}
 	}
 	_putchar('\n');
