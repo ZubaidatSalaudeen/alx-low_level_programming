a888d046af3b21a4b00bb6ded4bc02a6bd7b858d
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-07 01:56:02 +0000

    Debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if ((c > a && c > b) && ( a >= b || b >= a))
+	else if (c > a)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = a;
 	}
 	return (largest);
 }
