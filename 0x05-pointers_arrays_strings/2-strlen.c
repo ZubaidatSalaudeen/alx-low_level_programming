9237c330a528210facda79656da1e654b5cbc033
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-12 11:25:40 +0000

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ * Return: 0 for success
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
