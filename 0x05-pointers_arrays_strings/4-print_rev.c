35caf7b9a2b99747851a31552654d94eb19998c0
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-12 12:06:51 +0000

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a sring in reverse
+ * @s: the string
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, j;
+
+	j = strlen(s);
+	for (i = j; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
