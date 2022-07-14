c43b5a92f0cbd50678ab10031ada7e679fba4050
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-14 19:36:23 +0000

    string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
-
-	for (j = 0; src[j] != '\0' && n > 0; i++, n-- j++)
-		dest[i] = src[j];
+	int length = 0, c;
+	
+	while (dest[length] != '\0')
+		length++;
+	
+	for (c = 0; c < n ; c++, length++)
+		dest[length] = src[c];
+	
 	return (dest);
 }
