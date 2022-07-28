3fc02fd11fe070b3b8799749022d80958c798f04
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-07-28 17:46:10 +0100

    memmory allocatiob

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to be allocated
+ * Return: a pointer to tge allocated memory
+ * If mallic fails, normal process termination with a status value of 98 is caused
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
