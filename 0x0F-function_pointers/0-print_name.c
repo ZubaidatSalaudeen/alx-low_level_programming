ebbb7f28efd7c59bd32f8703c34818b4d0bbff6c
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-08-03 19:24:04 +0100

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,6 +2,7 @@
 /**
  * print_name - prints a name
  * @name: name to be printed
+ * @f: name is printed according to this function
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
