1350bb77d474e6d81776b259da7250938f584626
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-08-11 22:19:26 +0100

    freeing list_t list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LIST_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct list_s
 {
@@ -13,5 +14,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
